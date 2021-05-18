794e6f80884b3c7d5329fc046fc4192e60faa75f
Archit Singh <architsingh456@gmail.com>
2021-05-18 22:29:50 +0530

    UPD : added lambda expression to implement the interface method

--- src/main/java/com/github/lambdas/Greeter.java
+++ src/main/java/com/github/lambdas/Greeter.java
@@ -4,10 +4,19 @@ public class Greeter {
 
   public static void main(String[] args) {
     Greeter greeter = new Greeter();
+
+    //notice that the object is of type Action (Interface)
     Action actionImplementation = new ActionImplementation();
+
     //passing object of the implementation class of the interface
     //this method can now do anything according to the implementation class
     greeter.greet(actionImplementation);
+
+    //lambda expression of type Action (Interface)
+    Action performLambda = () ->
+      System.out.println("Im the perform lambda of the action class");
+
+    performLambda.perform();
   }
 
   //here I'm passing a behaviour to the greet method --- NO
@@ -16,8 +25,4 @@ public class Greeter {
   private void greet(Action action) {
     action.perform();
   }
-
-  //lambda expression of type action (Interface)
-  Action performLambda = () ->
-    System.out.println("Im the perform lambda of the action class");
 }
